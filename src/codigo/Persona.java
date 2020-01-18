4409f911ce8eed2b8c2781d889c0f952caf694a1
Adrian <proyectochipi@gmail.com>
2020-01-18 21:15:46 +0100

    Persona Final

--- src/codigo/Persona.java
+++ src/codigo/Persona.java
@@ -3,25 +3,102 @@ package codigo;
  * y lo introduzca en el atributo nombre, para posteriormente mostrar por pantalla 
  * el nuevo valor del atributo.*/
 public class Persona {
-	String nombre;
-	int edad;
-	float altura;
+	private String nombre;
+	private int edad;
+	private char sexo;
+	private String DNI;
+	private float altura,peso;
 public Persona() {
-	
+
 }
-public Persona(String nombre,int edad,float altura) {
+
+public Persona(String nombre,int edad,String DNI,char sexo) {
 	this.nombre=nombre;
 	this.edad=edad;
+	this.DNI=DNI;
+	this.sexo=sexo;
+}
+public Persona(String nombre,int edad,String DNI,char sexo,float altura,float peso) {
+	this.nombre=nombre;
+	this.edad=edad;
+	this.DNI=DNI;
+	this.sexo=sexo;
 	this.altura=altura;
+	this.peso=peso;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public int getEdad() {
+	return edad;
+}
+public void setEdad(int edad) {
+	this.edad = edad;
+}
+public char getSexo() {
+	return sexo;
+}
+public void setSexo(char sexo) {
+	this.sexo = sexo;
+}
+public String getDNI() {
+	return DNI;
+}
+public void setDNI(String dNI) {
+	DNI = dNI;
+}
+public float getAltura() {
+	return altura;
+}
+public void setAltura(float altura) {
+	this.altura = altura;
+}
+public float getPeso() {
+	return peso;
+}
+public void setPeso(float peso) {
+	this.peso = peso;
+}
+public float masa_Corporal() {
+	float masa_Corporal=peso/(altura*altura);
+	return masa_Corporal;
+}
+public String valoracion(double masa) {
+	String valoracion="";
+	float IMC=masa_Corporal();
+	if(IMC<20) {
+		valoracion="Peso por debajo de lo normal";
+	}else if(IMC>=25){
+		valoracion="Peso por encima de lo normal";
+	}else {
+		valoracion="Peso considerado normal";
+	}
+	return valoracion;
+}
+
+public boolean mayorEdad() {
+	return edad>=18?true:false;
 	
 }
-	String consulta_Nombre() {
-		return nombre;
-
-	}
-
-	void cambia_Nombre(String nom) {
-		nombre = nom;
-
+public void comprobarSexo(char sexo) {
+	if(sexo=='H'| sexo=='M') {
+		this.sexo=sexo;
+	}else {
+		this.sexo='H';
 	}
 }
+
+public String toString() {
+	return "Nombre: "+nombre+"\n"+
+			"Edad: "+edad+"\n"+
+			"DNI :"+DNI+"\n"+
+			"Sexo: "+sexo+"\n"+
+			"IMC: "+ masa_Corporal()+"\n"+
+			"Valoración: "+valoracion(masa_Corporal())+"\n";
+}
+
+}
+
--- src/codigo/main.java
+++ src/codigo/main.java
@@ -5,16 +5,48 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
+		int edad;
+		String nombre,DNI;
+		float peso,altura;
+		char sexo;
 		Scanner tec=new Scanner(System.in);
-		System.out.println("Introduce el nombre de la Persona");
-		Persona p1= new Persona();
-		String cad=tec.nextLine();
-		p1.cambia_Nombre(cad);
-		System.out.println(p1.consulta_Nombre());
-		System.out.println("Cambiogit");
-		System.out.println("Cambio 2");
-		System.out.println("CAmbio3");
-		System.out.println("Cambio final");
-	}
+		System.out.print("Introduce el nombre de la Persona");
+		nombre=tec.next();
+		System.out.print("\nIntroduce la edad");
+		edad=tec.nextInt();
+		System.out.print("\nIntroduce el DNI");
+		DNI=tec.next();
+		System.out.print("\nIntroduce el sexo(H=Hombre / M=Mujer): ");
+		sexo=tec.next().charAt(0);
+		System.out.print("\nIntroduce la altura en metros: ");
+		altura=tec.nextFloat();
+		System.out.print("\nIntroduce el peso en kilogramos: ");
+		peso=tec.nextFloat();
+		
+		Persona p1= new Persona(nombre,edad,DNI,sexo,altura,peso);
+		Persona p2=new Persona(nombre,edad,DNI,sexo);
+		Persona p3=new Persona();
+		p3.setNombre(nombre);
+		p3.setEdad(edad);
+		p3.setDNI(DNI);
+		p3.setSexo(sexo);
+		p3.setAltura(altura);
+		p3.setPeso(peso);
+		
+		esMayorEdad(p1);
+		esMayorEdad(p2);
+		esMayorEdad(p3);
+		System.out.println("Persona 1: \n"+p1.toString());
+		System.out.println("Persona 2: \n"+p2.toString());
+		System.out.println("Persona 3: \n"+p3.toString());
+		tec.close();
 
+	}
+	private static void esMayorEdad(Persona p1) {
+	if(p1.mayorEdad()==true) {
+		System.out.println("Es mayor");
+	}else {
+		System.out.println("Es menor");
+	}
+	}
 }
